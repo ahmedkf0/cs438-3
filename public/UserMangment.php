903ed20e2e8b7de8d44492238c95c5321a238e1e
AbdulftahAdel <abdulftahelmontser@gmail.com>
2024-12-13 23:46:30 +0200

    واجهة تحديد تعديل او حدف مستخدم

---
+++ public/UserMangment.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>إدارة المستخدمين</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <main class="selection-container">
+        <div class="options">
+        <a href="Selection.html" style="display: inline-block; margin-bottom: 20px; padding: 10px 15px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px;">
+            الرجوع للخلف
+        </a>
+            <h1>إدارة المستخدم</h1>
+            <a href="UserEdit.php" class="option">التعديل في بيانات المستخدم</a>
+            <a href="UserDelete.php" class="option">حذف المستخدم</a> 
+        </div>
+    </main>
+</body>
+</html>
